01a6ba9cf5de5ef111403d70f042c2c2abb3cd81
mrfadlika <mrfadlika248@gmail.com>
2024-11-03 15:39:32 +0800

    Tugas Overloading dan Overriding

--- Main.java
+++ Main.java
@@ -1,74 +1,139 @@
-class OperasiMahasiswa {
-    public int tambahNilai(int nilai1, int nilai2) {
-        int hasilTambah = nilai1 + nilai2;
-        System.out.println("Penjumlahan Integer: " + hasilTambah);
-        return hasilTambah;
+class kelasCalculatorKu {
+    protected int x1, x2;
+    protected double y1, y2;
+
+    public kelasCalculatorKu(int x1, int x2, double y1, double y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
     }
 
-    public double tambahNilaiDouble(double nilai1, double nilai2) {
-        double hasilTambah = nilai1 + nilai2;
-        System.out.println("Penjumlahan Double: " + hasilTambah);
-        return hasilTambah;
+    public int ambilkiHasilIntegernya() {
+        return 0;
     }
 
-    public int kurangNilai(int nilai1, int nilai2) {
-        int hasilKurang = nilai1 - nilai2;
-        System.out.println("Pengurangan Integer: " + hasilKurang);
-        return hasilKurang;
+    public double ambilkiHasilDoublenya() {
+        return 0.0;
+    }
+}
+
+class Penjumlahan extends kelasCalculatorKu {
+
+    public Penjumlahan(int x1, int x2) {
+        super(x1, x2, 0, 0);
     }
 
-    public double kurangNilaiDouble(double nilai1, double nilai2) {
-        double hasilKurang = nilai1 - nilai2;
-        System.out.println("Pengurangan Double: " + hasilKurang);
-        return hasilKurang;
+    public Penjumlahan(double y1, double y2, boolean isDouble) {
+        super(0, 0, y1, y2);
     }
 
-    public int kaliNilai(int nilai1, int nilai2) {
-        int hasilKali = nilai1 * nilai2;
-        System.out.println("Perkalian Integer: " + hasilKali);
-        return hasilKali;
+    @Override
+    public int ambilkiHasilIntegernya() {
+        return x1 + x2;
     }
 
-    public double kaliNilaiDouble(double nilai1, double nilai2) {
-        double hasilKali = nilai1 * nilai2;
-        System.out.println("Perkalian Double: " + hasilKali);
-        return hasilKali;
+    @Override
+    public double ambilkiHasilDoublenya() {
+        return y1 + y2;
+    }
+}
+
+class Pengurangan extends kelasCalculatorKu {
+
+    public Pengurangan(int x1, int x2) {
+        super(x1, x2, 0, 0);
     }
 
-    public void bagiNilai(int nilai1, int nilai2) {
-        if (nilai2 != 0) {
-            System.out.println("Pembagian Integer: " + (nilai1 / nilai2));
+    public Pengurangan(double y1, double y2, boolean isDouble) {
+        super(0, 0, y1, y2);
+    }
+
+    @Override
+    public int ambilkiHasilIntegernya() {
+        return x1 - x2;
+    }
+
+    @Override
+    public double ambilkiHasilDoublenya() {
+        return y1 - y2;
+    }
+}
+
+class Perkalian extends kelasCalculatorKu {
+
+    public Perkalian(int x1, int x2) {
+        super(x1, x2, 0, 0);
+    }
+
+    public Perkalian(double y1, double y2, boolean isDouble) {
+        super(0, 0, y1, y2);
+    }
+
+    @Override
+    public int ambilkiHasilIntegernya() {
+        return x1 * x2;
+    }
+
+    @Override
+    public double ambilkiHasilDoublenya() {
+        return y1 * y2;
+    }
+}
+
+class Pembagian extends kelasCalculatorKu {
+
+    public Pembagian(int x1, int x2) {
+        super(x1, x2, 0, 0);
+    }
+
+    public Pembagian(double y1, double y2, boolean isDouble) {
+        super(0, 0, y1, y2);
+    }
+
+    @Override
+    public int ambilkiHasilIntegernya() {
+        if(x2 != 0) {
+            return x1 / x2;
         } else {
-            System.out.println("Pembagian Integer: Error (Penyebut-nya 0)");
+            return 0;
         }
     }
 
-    public void bagiNilaiDouble(double nilai1, double nilai2) {
-        if (nilai2 != 0) {
-            System.out.println("Pembagian Double: " + (nilai1 / nilai2));
+    @Override
+    public double ambilkiHasilDoublenya() {
+        if (y2 != 0) {
+            return y1 / y2;
         } else {
-            System.out.println("Pembagian Double: Error (Penyebut-nya 0)");
+            return 0;
         }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        OperasiMahasiswa operasi = new OperasiMahasiswa();
+        Penjumlahan addInt = new Penjumlahan(10, 5);
+        System.out.println("Hasil Penjumlahan Integer: " + addInt.ambilkiHasilIntegernya());
 
-        operasi.tambahNilai(5, 3);
-        operasi.tambahNilaiDouble(5.5, 3.3);
+        Penjumlahan addDouble = new Penjumlahan(2.5, 3.5, true);
+        System.out.println("Hasil Penjumlahan Double: " + addDouble.ambilkiHasilDoublenya());
 
-        operasi.kurangNilai(5, 3);
-        operasi.kurangNilaiDouble(5.5, 3.3);
+        Pengurangan subsInt = new Pengurangan(10, 5);
+        System.out.println("\nHasil Pengurangan Integer: " + subsInt.ambilkiHasilIntegernya());
 
-        operasi.kaliNilai(5, 3);
-        operasi.kaliNilaiDouble(5.5, 3.3);
+        Pengurangan subsDouble = new Pengurangan(10.1, 5.6, true);
+        System.out.println("Hasil Pengurangan Double: " + subsDouble.ambilkiHasilDoublenya());
 
-        operasi.bagiNilai(10, 2);
-        operasi.bagiNilai(10, 0);
+        Perkalian mulInt = new Perkalian(10, 5);
+        System.out.println("\nHasil Perkalian Integer: " + mulInt.ambilkiHasilIntegernya());
 
-        operasi.bagiNilaiDouble(10.5, 2);
-        operasi.bagiNilaiDouble(10.5, 0);
+        Perkalian mulDouble = new Perkalian(17.1, 1.9, true);
+        System.out.println("Hasil Perkalian Double: " + mulDouble.ambilkiHasilDoublenya());
+
+        Pembagian devInt = new Pembagian(10, 5);
+        System.out.println("\nHasil Pembagian Integer: " + devInt.ambilkiHasilIntegernya());
+
+        Pembagian devDouble = new Pembagian(4.2, 1.6, true);
+        System.out.println("Hasil Pembagian Double: " + devDouble.ambilkiHasilDoublenya());
     }
 }
